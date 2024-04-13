32c4447ade38857b4bd868aa5cef88f0a74eaeb0
karaozonur <karaoz.onr@gmail.com>
2024-04-13 20:25:53 +0300

    Add project files.

---
+++ WEBAPP-Signalr-ile-qr-kodlu-siparis-yonetimi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEBAPP_Signalr_ile_qr_kodlu_siparis_yonetimi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
